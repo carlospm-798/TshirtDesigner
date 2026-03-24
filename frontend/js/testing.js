ea0621a2e7f228cb3c97123414758bf8c12f2bae
Carlos Paredes <carlos.paredes6900@alumnos.udg.mx>
2026-03-24 00:31:28 -0600

    Adding some logs to check the basic conections of the page

---
+++ frontend/js/testing.js
@@ -0,0 +1,10 @@
+var create_lobby_btn = document.getElementById("generate_lobby")
+var enter_lobby_btn = document.getElementById("enter_lobby")
+
+create_lobby_btn.onclick = function() {
+  console.log('creating lobby...')
+}
+
+enter_lobby_btn.onclick = function () {
+  console.log('entering to a lobby...')
+}
