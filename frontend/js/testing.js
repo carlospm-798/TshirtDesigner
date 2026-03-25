f75b91bb94aaa526a309a2668014c6cb2c5416bd
Carlos Paredes <carlos.paredes6900@alumnos.udg.mx>
2026-03-25 12:36:08 -0600

    Adding new comments is the JS file

--- frontend/js/testing.js
+++ frontend/js/testing.js
@@ -1,18 +1,36 @@
+/*  ---------------------------------------------------   #
+#   The TShirt-Designer game, by Carlos Paredes Márquez   #
+#   ---------------------------------------------------   */
+
+//  These are the main global id to implement the game actions      //
 var create_lobby_btn  = document.getElementById("generate_lobby")
 var enter_lobby_btn   = document.getElementById("enter_lobby")
 var main_window       = document.getElementById("main_window")
+//  -------------------------------------------------------------   //
 
-create_lobby_btn.onclick = function() {
+
+//  These are the main functions to implement the game actions    //
+create_lobby_btn.onclick = function () {
+  //  This function starts a new lobby and hide the main
+  //  Interface of the web page, except the body
   console.log('creating lobby...')
   startLobbyCreation();
   main_window.style.display = 'none';
 }
+//  ----------------------------------------------------------    //
+
+
 
 enter_lobby_btn.onclick = function () {
+  //  This function enter to a lobby and hide the main
+  //  Interface of the web page, except the body
   console.log('entering to a lobby...')
   enterLobby();
   main_window.style.display = 'none';
 }
+//  ----------------------------------------------------------    //
+
+
 
 async function startLobbyCreation() {
   try {
