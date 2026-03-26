a3a267cf8ce935e3f48a1bc77a2d17f328aa521a
Carlos Paredes <carlos.paredes6900@alumnos.udg.mx>
2026-03-25 18:05:07 -0600

    Saving backend files before start testing

--- backend/src/game_server/main.py
+++ backend/src/game_server/main.py
@@ -1,2 +1,46 @@
-if __name__ == '__main__':
-    print("Hello world\n")
+from fastapi import FastAPI, WebSocket, WebSocketDisconnect
+from fastapi.middleware.cors import CORSMiddleware
+from lobby_manager import create_lobby, join_lobby, lobbies
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
+
+@app.post("/create-lobby")
+async def create(data: dict):
+    lobby, player_id = create_lobby(data["username"])
+    return {
+        "lobby_code": lobby.code,
+        "player_id": player_id
+    }
+
+@app.post("/join-lobby")
+async def join(data: dict):
+    lobby, player_id = join_lobby(data["code"], data["username"])
+    if not lobby:
+        return {"error": "Lobby not found"}
+
+    return {
+        "player_id": player_id
+    }
+
+@app.websocket("/ws/{code}")
+async def websocket_endpoint(ws: WebSocket, code: str):
+    await ws.accept()
+
+    try:
+        while True:
+            data = await ws.receive_text()
+            await ws.send_json({
+                "players": [
+                    {"name": p.name, "is_host": p.is_host}
+                    for p in lobbies[code].players.values()
+                ]
+            })
+    except WebSocketDisconnect:
+        print("Client diconnected")
