3b8fee1fae51f319a5945bbdae1e8e12099c060d
Carlos Paredes <carlos.paredes6900@alumnos.udg.mx>
2026-03-31 13:28:57 -0600

    Saving the resent refactor, and waitting for another one to separate functions from the DOM

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -1,8 +1,6 @@
-/*  Carlos Paredes Márquez
-    This JavaScript is the one that must manage
-    The main behaviour of the web-page game.
-*/
-
+/*  Carlos Paredes Márquez                          #
+#   This JavaScript is the one that must manage     #
+#   The main behaviour of the web-page game.       */
 //  This import take the web-sockets actions, and helps us
 //  to separate the main actions vs the socket actions, letting
 //  cleaner scripts, easily to maintain.
@@ -17,26 +15,22 @@ document.addEventListener("DOMContentLoaded", async () => {
   // Principal buttons
   const create_lobby_btn = document.getElementById("generate_lobby");
   const enter_lobby_btn  = document.getElementById("enter_lobby");
-
   // Screens
   const main_window = document.getElementById("main_window");
   const lobby       = document.getElementById("lobby");
-
   // Forms
   const form = document.querySelector(".input_form_class");
   const users_list = document.querySelector(".users_list");
-
   // Inputs
   const uname = document.getElementById("uname");
   const lcode = document.getElementById("lcode");
   const enter_data_btn = document.getElementById("input_username_btn");
-
+  const start_game_btn = document.getElementById("start_game_btn");
   //  Global variables
   let lobbyMode = null;
   let playerId = null;
   let API_URL = "";
   let WS_URL = "";
-
   //  LOAD CONFIG FIRST
   const protocol = window.location.protocol === "https:" ? "wss" : "ws";
   const host = window.location.host;
@@ -59,7 +53,7 @@ document.addEventListener("DOMContentLoaded", async () => {
 
 
 
-  /* --------- BUTTONS MANAGER --------- */
+  /* --------- CREATE LOBBY BUTTON MANAGER --------- */
   create_lobby_btn.onclick = () => {
     //  This function manage the create lobby actions of The    //
     //  main screan, by lock the lobby code form, to show the   //
@@ -69,7 +63,11 @@ document.addEventListener("DOMContentLoaded", async () => {
     lcode.disabled = true;
     lcode.value = "----";
   };
+  //  -----------------------------------------------------     //
 
+
+
+  /* --------- ENTER LOBBY BUTTON MANAGER --------- */
   enter_lobby_btn.onclick = () => {
     //  This function manage the enter lobby actions of The     //
     //  main screan, by unlock the lobby code form, to input    //
@@ -110,6 +108,46 @@ document.addEventListener("DOMContentLoaded", async () => {
 
 
 
+  /* --------- IS VALID USERNAME MANAGER --------- */
+  function isValidUsername(username) {
+    //  This function returns a bool based in a correct
+    //  username without blank spaces
+    return username && !username.includes(" ");
+  }
+  //  -----------------------------------------------------     //
+
+
+
+  /* --------- CREATE LOBBY MANAGER --------- */
+  async function createLobby(username) {
+    //  This function manage the creation of the api to create
+    //  a lobby, based in the host username.
+    const response = await fetch(`${API_URL}/create-lobby`, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ username })
+    });
+    return response.json();
+  }
+  //  -----------------------------------------------------     //
+
+
+
+  /* --------- JOIN LOBBY MANAGER --------- */
+  async function joinLobby(username, code) {
+    //  This function manage the join in a lobby, based
+    //  in the username and the code of the host lobby.
+    const response = await fetch(`${API_URL}/join-lobby`, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ username, code })
+    });
+    return response.json();
+  }
+  //  -----------------------------------------------------     //
+
+
+
   /* --------- SUBMIT --------- */
   form.addEventListener("submit", async (e) => {
     //  This function helps to avoid issues as for example, adding        //
@@ -118,56 +156,30 @@ document.addEventListener("DOMContentLoaded", async () => {
     e.preventDefault();
 
     const username = uname.value.trim();
-    //  Validation of not blank spaces in the username
-    if (!username || username.includes(" ")) {
+    if (!isValidUsername(username)) {
+      //  Alert in case of blank spaces usage
       alert("Invalid username");
       return;
     }
 
-    let response;
-    let lobbyCode;
+    //  Local variables to save code and messages
+    let data; let lobbyCode;
 
-    if (lobbyMode === "create") {
-      //  Validation of the create lobby mode
-      response = await fetch(`${API_URL}/create-lobby`, {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({ username })
-      });
-
-      const data = await response.json();
-
-      playerId = data.player_id;
-      updateUsersList(data.players);
-
-      lobbyCode = data.lobby_code;
-
+    if (lobbyCode === "create") {
+      //  Check the page logic in case of HOST
+      data = await createLobby(username);
+      lobbyCode = data.lobbyCode;
       lcode.value = lobbyCode;
-      enter_data_btn.disabled = true;
+      start_game_btn.removeAttribute("hidden");
     }
 
     if (lobbyMode === "join") {
-      //  Validation of the join lobby mode
+      //  Check the page logic and join in case of user
       lobbyCode = lcode.value.trim().toUpperCase();
-
-      response = await fetch(`${API_URL}/join-lobby`, {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({ username, code: lobbyCode })
-      });
-
-      const data = await response.json();
-
-      playerId = data.player_id;
-      updateUsersList(data.players);
+      data = await joinLobby(username, lobbyCode);
     }
 
-    if (!playerId) {
-      console.error("PlayerId is null - WS connection blocked");
-      return;
-    }
-
-    //  Handle the lobby conections and updating the new interface
+    //  Connect/Disconnect to lobby and show the next screen stage
     connectToLobby(WS_URL, lobbyCode, playerId, updateUsersList, handleLobbyClosed);
     show_screen(lobby);
   });
@@ -175,36 +187,60 @@ document.addEventListener("DOMContentLoaded", async () => {
 
 
 
+  /* -----  CREATE PLAYER NAME  -----*/
+  function createPlayerName(name) {
+    //  This function manage the creation of player name
+    //  to refactor and clean the flow of the game.
+    const span = document.createElement("span");
+    span.textContent = name;
+    span.classList.add("player_name");
+    return span;
+  }
+  //  -----------------------------------------------------     //
+
+
+
+  /* -----  CREATE HOST BADGE  -----*/
+  function createHostBadge() {
+    //  This function manage the creation of a badge
+    //  to easily identify the HOST of the seasion.
+    const badge = document.createElement("span");
+    badge.textContent = "HOST";
+    badge.classList.add("host_badge");
+    return badge;
+  }
+  //  -----------------------------------------------------     //
+
+
+
+  /* -----  CREATE PLAYER ITEM  -----*/
+  function createPlayerItem(player) {
+    //  This function manage the creation of a HTML
+    //  li element of the players, based in its
+    //  conditions as normal/host player, then it
+    //  returns a li html element.
+    const li = document.createElement("li");
+
+    if (player.is_host) {
+      //  invoke the host function badge and
+      //  append it to the li element.
+      li.appendChild(createHostBadge());
+    }
+
+    //  append the player name to the li element
+    li.appendChild(createPlayerName(player.name));
+    return li;
+  }
+  //  -----------------------------------------------------     //
+
+
+
   /* --------- UI UPDATE --------- */
   function updateUsersList(players) {
-    //  This function helps to update the user list in the lobby    //
-    //  page, by adding new childs to its elements, it also works   //
-    //  the distinction of the HOST vs the USERS.                   //
     users_list.innerHTML = "";
 
     players.forEach(player => {
-      const li = document.createElement("li");
-
-      if (player.is_host) {
-        const badge = document.createElement("span");
-        badge.textContent = "HOST";
-        badge.classList.add("host_badge");
-
-        const name = document.createElement("span");
-        name.textContent = player.name;
-        name.classList.add("player_name");
-
-        li.appendChild(badge);
-        li.appendChild(name);
-      } else {
-
-        const name = document.createElement("span");
-        name.textContent = player.name;
-        name.classList.add("player_name");
-        li.appendChild(name);
-      }
-
-      users_list.appendChild(li);
+      users_list.appendChild(createPlayerItem(player));
     });
   }
   //  -----------------------------------------------------     //
